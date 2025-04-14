3b3dddb9c68e7bedd9bb4f0901e033c969d169b6
Nitin2104 <143647072+Nitin2104@users.noreply.github.com>
2025-04-14 13:04:36 +0530

    Update Test.java

--- GithubProject/src/com/flm/Test.java
+++ GithubProject/src/com/flm/Test.java
@@ -7,9 +7,6 @@ public class Test {
 
 	}
 	
-	public static void sayHi() {
-		System.out.println("Hi..., I am Developer-1");
-	}
 	
 	public static void sayBye() {
 		System.out.println("Bye..., i am Developer-2");
